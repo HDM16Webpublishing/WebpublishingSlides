a63796e902360f958e1a36d2bca8de150f17641f
Benjamin Zapilko <zapilko@gmx.de>
2016-12-05 15:04:38 +0100

    Update

---
+++ common/hdm-scripts.js
@@ -0,0 +1,38 @@
+var y = document.querySelectorAll("pre code");
+for(var i = 0; i < y.length; i++) {
+  y[i].innerHTML = y[i].innerHTML.replace("\n", "");
+}
+
+
+
+(function() {
+    var startingTime = new Date().getTime();
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 20);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        $(function() {
+            var endingTime = new Date().getTime();
+            var tookTime = endingTime - startingTime;
+	    jQuery.getScript('common/jquery.zoomooz.min.js', function() {
+		$('img').zoomTarget();
+	    });
+        });
+    });
+})();
+
+
